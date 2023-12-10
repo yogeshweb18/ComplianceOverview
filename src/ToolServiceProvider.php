c6caa5249bc1f017790d78522d5a24c85db70809
unknown <yogesh.thakur@acc.ltd>
2023-12-10 12:39:46 +0530

    start

---
+++ src/ToolServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Axistrustee\ComplianceOverview;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Http\Middleware\Authenticate;
+use Laravel\Nova\Nova;
+use Axistrustee\ComplianceOverview\Http\Middleware\Authorize;
+
+class ToolServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->booted(function () {
+            $this->routes();
+        });
+
+ 
+    }
+
+    /**
+     * Register the tool's routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        if ($this->app->routesAreCached()) {
+            return;
+        }
+
+          
+    }
+
+    /**
+     * Register any application services.
+     * 
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
